1f31404751d055292ff6d2d9c5718ac11493c325
UraniumRockers <skandarajbhuvanesh@gmail.com>
2024-11-07 12:06:04 -0500

    Tablet (day 3)
    Got the tablet text to pop up when interacted, started on making it make box colliders disappear

--- Ascent To Hell (A Sisyphus Game)/Assets/Scripts/PlayerLevel2D.cs
+++ Ascent To Hell (A Sisyphus Game)/Assets/Scripts/PlayerLevel2D.cs
@@ -9,51 +9,54 @@ public class PlayerLevel2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Tablet.shouldPlayerMove)
         {
-            if (Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.W))
             {
-                transform.position += speed * Time.deltaTime * new Vector3(1, 1, 0);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * -45), 200f);
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += speed * Time.deltaTime * new Vector3(1, 1, 0);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * -45), 200f);
+                }
+                else if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position += speed * Time.deltaTime * new Vector3(-1, 1, 0);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * 45), 200f);
+                }
+                else
+                {
+                    transform.position += speed * Time.deltaTime * Vector3.up;
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.zero), 200f);
+                }
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.position += speed * Time.deltaTime * new Vector3(1, -1, 0);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * -135), 200f);
+                }
+                else if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position += speed * Time.deltaTime * new Vector3(-1, -1, 0);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * 135), 200f);
+                }
+                else
+                {
+                    transform.position += speed * Time.deltaTime * Vector3.down;
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * 180), 200f);
+                }
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += speed * Time.deltaTime * Vector3.right;
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * -90), 200f);
             }
             else if (Input.GetKey(KeyCode.A))
             {
-                transform.position += speed * Time.deltaTime * new Vector3(-1, 1, 0);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * 45), 200f);
+                transform.position += speed * Time.deltaTime * Vector3.left;
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * 90), 200f);
             }
-            else
-            {
-                transform.position += speed * Time.deltaTime * Vector3.up;
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.zero), 200f);
-            }
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.position += speed * Time.deltaTime * new Vector3(1, -1, 0);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * -135), 200f);
-            }
-            else if (Input.GetKey(KeyCode.A))
-            {
-                transform.position += speed * Time.deltaTime * new Vector3(-1, -1, 0);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * 135), 200f);
-            }
-            else
-            {
-                transform.position += speed * Time.deltaTime * Vector3.down;
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * 180), 200f);
-            }
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += speed * Time.deltaTime * Vector3.right;
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * -90), 200f);
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += speed * Time.deltaTime * Vector3.left;
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(Vector3.forward * 90), 200f);
         }
     }
 }
--- Ascent To Hell (A Sisyphus Game)/Assets/Scripts/Tablet.cs
+++ Ascent To Hell (A Sisyphus Game)/Assets/Scripts/Tablet.cs
@@ -1,14 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
 
 public class Tablet : MonoBehaviour
 {
-    private void OnCollisionStay2D(Collision2D collision)
+    public static bool shouldPlayerMove = true;
+
+    //BoxCollider2D originBoxCollider = GameObject.Find("Origin").GetComponent<BoxCollider2D>();
+    GameObject tabletCanvas; 
+    TMP_Text tabletText;
+    bool keyDown = false;
+    bool activatedForFirstTime = false;
+    // MAKE THE BOX COLLIDER FOR THE RED CLOUD DISAPPEAR AFTER IT IS ACTIVATED FOR THE FIRST TIME
+
+    private void Awake()
     {
-        if (collision.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        tabletCanvas = GameObject.Find("Level 1 Tablet Canvas");
+        tabletText = tabletCanvas.GetComponentsInChildren<TMP_Text>()[0];
+        tabletText.text = "This is your new life. Explore while you still have the chance. When you are ready, venture upwards. The path will reveal itself, and the pressure plate to summon your punishment will be visible.";
+        tabletCanvas.SetActive(false);
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            // display the text and stuff
+            keyDown = true;
+        }
+        shouldPlayerMove = !tabletCanvas.activeSelf;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && keyDown)
+        {
+            tabletCanvas.SetActive(!tabletCanvas.activeSelf);
+            keyDown = false;
         }
     }
 }
