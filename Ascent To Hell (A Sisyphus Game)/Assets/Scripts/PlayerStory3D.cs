66a6d12b526cf869fb2ee81b30cf008e285c813d
UraniumRockers <skandarajbhuvanesh@gmail.com>
2025-01-12 23:08:40 -0500

    idk
    i did smthn but idr

--- Ascent To Hell (A Sisyphus Game)/Assets/Scripts/PlayerStory3D.cs
+++ Ascent To Hell (A Sisyphus Game)/Assets/Scripts/PlayerStory3D.cs
@@ -13,61 +13,87 @@ public class PlayerStory3D : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private Animation copAttack;
     private int sceneIndex;
+    private float playerRotationX;
+    private bool hasAnimStarted;
 
     // Start is called before the first frame update
     void Start()
     {
-        isCopAttackOver = false;
-        sceneIndex = SceneManager.GetActiveScene().buildIndex;
-        
-        switch (sceneIndex)
+        if (gameObject.name == "Player")
         {
-            case 3:
-                ObjectiveManager3D.SetObjectiveText("Get out of here.");
-                canPlayerMove = true;
-                break;
-            case 6:
-                ObjectiveManager3D.SetObjectiveText("Drop it.");
-                canPlayerMove = true;
-                break;
-            case 9:
-                ObjectiveManager3D.SetObjectiveText("What have you done...");
-                canPlayerMove = false;
-                break;
+            isCopAttackOver = false;
+            hasAnimStarted = false;
+            sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            playerRotationX = transform.rotation.x;
+
+            switch (sceneIndex)
+            {
+                case 3:
+                    ObjectiveManager3D.SetObjectiveText("Get out of here.");
+                    canPlayerMove = true;
+                    break;
+                case 6:
+                    ObjectiveManager3D.SetObjectiveText("Drop it.");
+                    canPlayerMove = true;
+                    break;
+                case 9:
+                    ObjectiveManager3D.SetObjectiveText("What have you done...");
+                    canPlayerMove = false;
+                    break;
+            }
         }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (canPlayerMove)
+        if (gameObject.name == "Player")
         {
-            if (Input.GetKey(KeyCode.W) && transform.position.z <= 11.5)
+            if (canPlayerMove)
             {
-                transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+                if (Input.GetKey(KeyCode.W) && transform.position.z <= 11.5)
+                {
+                    transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+                }
+                if (Input.GetKey(KeyCode.S) && transform.position.z >= -8.468307)
+                {
+                    transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+                }
             }
-            if (Input.GetKey(KeyCode.S) && transform.position.z >= -11)
-            {
-                transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
-            }
-        }
 
-        if (transform.position.z >= 11.5)
-        {
-            GameObject cop = GameObject.Find("Cop With Baton Anim");
-            if (cop.transform.position.z > 13.5)
+            if (transform.position.z >= 11.5)
             {
-                canPlayerMove = false;
-                cop.transform.Translate(new Vector3(0, 0, 0.5f));
+                GameObject cop = GameObject.Find("Cop With Baton Anim");
+                if (cop.transform.position.z > 13.5)
+                {
+                    canPlayerMove = false;
+                    cop.transform.Translate(new Vector3(0, 0, 0.5f));
+                }
+                else
+                {
+                    cop.transform.position = new Vector3(0, -.8f, 13.5f);
+                    cop.GetComponent<Animator>().Play("CopAttack");
+                }
+                if (isCopAttackOver)
+                {
+                    print("done");
+                }
             }
-            else
-            {
-                cop.transform.position = new Vector3(0, -.8f, 13.5f);
-                cop.GetComponent<Animator>().Play("CopAttack");
-            }
+
             if (isCopAttackOver)
             {
-                print("done");
+
+                print(transform.rotation.x);
+                if (transform.rotation.x > playerRotationX - .5)
+                {
+                    if (!hasAnimStarted)
+                    {
+                        GameObject.Find("Red").GetComponent<Animator>().Play("Red");
+                    }
+                    transform.Rotate(new Vector3(-30 * Time.deltaTime, -5 * Time.deltaTime, -5 * Time.deltaTime));
+                    print("player rotated");
+                }
             }
         }
     }
