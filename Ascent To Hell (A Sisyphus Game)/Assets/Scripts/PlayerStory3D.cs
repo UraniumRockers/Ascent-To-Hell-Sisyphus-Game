74f0b9c42d39e877d1eb83423b7b3bffb06103f9
UraniumRockers <skandarajbhuvanesh@gmail.com>
2025-01-13 12:05:43 -0500

    anim
    need to make at end of player turn anim the player calls a function from the script that disables the shouldlook var and enables the holdingbat var
    
    need to get playuer turn anim working

--- Ascent To Hell (A Sisyphus Game)/Assets/Scripts/PlayerStory3D.cs
+++ Ascent To Hell (A Sisyphus Game)/Assets/Scripts/PlayerStory3D.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.PackageManager;
+
+//using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -11,20 +14,35 @@ public class PlayerStory3D : MonoBehaviour
     private static bool isCopAttackOver = false;
 
     [SerializeField] private float speed;
+    [SerializeField] private GameObject redScreen;
     [SerializeField] private Animation copAttack;
+    [SerializeField] private GameObject health0;
+    [SerializeField] private GameObject health1;
     private int sceneIndex;
     private float playerRotationX;
     private bool hasAnimStarted;
+    private bool shouldPlayerLookBack;
+    private bool hasRedScreenFinished;
+    private bool isPlayerHoldingBat;
+    private bool hasPlayerTurnedBack;
 
     // Start is called before the first frame update
     void Start()
     {
+        print(transform.rotation.y);
+        GameObject.Find("Baseball Bat").GetComponent<Rigidbody>().useGravity = false;
+        //print("Gravity is false");
+
         if (gameObject.name == "Player")
         {
+            hasRedScreenFinished = false;
             isCopAttackOver = false;
             hasAnimStarted = false;
             sceneIndex = SceneManager.GetActiveScene().buildIndex;
             playerRotationX = transform.rotation.x;
+            isPlayerHoldingBat = false;
+            shouldPlayerLookBack = false;
+            hasPlayerTurnedBack = false;
 
             switch (sceneIndex)
             {
@@ -34,11 +52,13 @@ public class PlayerStory3D : MonoBehaviour
                     break;
                 case 6:
                     ObjectiveManager3D.SetObjectiveText("Drop it.");
-                    canPlayerMove = true;
+                    canPlayerMove = false;
+                    isPlayerHoldingBat = true;
                     break;
                 case 9:
                     ObjectiveManager3D.SetObjectiveText("What have you done...");
                     canPlayerMove = false;
+                    shouldPlayerLookBack = true;
                     break;
             }
         }
@@ -61,6 +81,23 @@ public class PlayerStory3D : MonoBehaviour
                     transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
                 }
             }
+            if (shouldPlayerLookBack)
+            {
+
+                
+            }
+
+
+            if (isPlayerHoldingBat)
+            {
+                if (Input.GetKeyDown(KeyCode.G))
+                {
+                    //print("Gravity is true");
+                    GameObject.Find("Baseball Bat").GetComponent<Rigidbody>().useGravity = true;
+                    canPlayerMove = true;
+                    ObjectiveManager3D.SetObjectiveText("Get out of here.");
+                }
+            }
 
             if (transform.position.z >= 11.5)
             {
@@ -74,32 +111,47 @@ public class PlayerStory3D : MonoBehaviour
                 {
                     cop.transform.position = new Vector3(0, -.8f, 13.5f);
                     cop.GetComponent<Animator>().Play("CopAttack");
+                    ObjectiveManager3D.SetObjectiveText("They know.");
                 }
                 if (isCopAttackOver)
                 {
                     print("done");
                 }
+
             }
 
             if (isCopAttackOver)
             {
-
-                print(transform.rotation.x);
-                if (transform.rotation.x > playerRotationX - .5)
+                
+                health1.SetActive(false);
+                health0.SetActive(true);
+                //print(transform.rotation.x);
+                if (transform.rotation.x > playerRotationX - .5 && !hasRedScreenFinished)
                 {
+                    //print("Rotate");
                     if (!hasAnimStarted)
                     {
-                        GameObject.Find("Red").GetComponent<Animator>().Play("Red");
+                        redScreen.GetComponent<Animator>().Play("Red Screen");
+
                     }
                     transform.Rotate(new Vector3(-30 * Time.deltaTime, -5 * Time.deltaTime, -5 * Time.deltaTime));
-                    print("player rotated");
+                    //print("player rotated");
                 }
             }
         }
+
     }
 
     public void CopAttackOver()
     {
         isCopAttackOver = true;
     }
+
+    public void RedScreenOver()
+    {
+        //print("this ran");
+        hasRedScreenFinished = true;
+        SceneManager.LoadScene(4);
+    }
+
 }
